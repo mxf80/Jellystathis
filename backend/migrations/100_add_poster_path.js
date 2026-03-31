4a4125df3fd11e6fb7e8908e2fde2232306d7158
alex goncharov <47995525+mxf80@users.noreply.github.com>
2026-03-31 16:04:08 +0300

    Add 'PosterPath' column to jf_library_items table
    Add migration to create and drop 'PosterPath' column in 'jf_library_items' table.

--- backend/migrations/100_add_poster_path.js
+++ backend/migrations/100_add_poster_path.js
@@ -1,1 +1,27 @@
+exports.up = async function (knex) {
+  try {
+    const hasColumn = await knex.schema.hasColumn("jf_library_items", "PosterPath");
 
+    if (!hasColumn) {
+      await knex.schema.table("jf_library_items", function (table) {
+        table.text("PosterPath");
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.down = async function (knex) {
+  try {
+    const hasColumn = await knex.schema.hasColumn("jf_library_items", "PosterPath");
+
+    if (hasColumn) {
+      await knex.schema.table("jf_library_items", function (table) {
+        table.dropColumn("PosterPath");
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
